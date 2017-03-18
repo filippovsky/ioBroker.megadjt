b05206a0a0fd29f6758d97eb1a7aa0abf8eb19d5
filippovsky <filippovsky@inbox.ru>
2017-03-18 21:56:53 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -227,7 +227,7 @@ function updateFirmware( ) {
 
    //cmd = '/usr/bin/php '+dir+'/www/megad-cfg-2561.php --fw '+dir+'/www/megad-2561.hex -p '+pass+' --ee --ip '+ip;
    //cmd = dir+'/www/megad-cfg-2561.php --fw '+dir+'/www/megad-2561.hex -p '+pass+' --ee --ip '+ip;
-   cmd = 'cd '+dir+'/www|php megad-cfg-2561.php --fw megad-2561.hex -p '+pass+' --ee --ip '+ip;
+   cmd = 'cd '+dir+'|megad-cfg-2561.php --fw megad-2561.hex -p '+pass+' --ee --ip '+ip;
 
    adapter.log.debug(cmd);
 
