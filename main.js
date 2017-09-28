9fc9f2c90f720de7e1a66a0f8dc0cf56aec86ea5
Filippovsky <filippovsky@inbox.ru>
2017-09-28 23:27:07 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ var ports  = {};
 ///var askInternalTemp = false;
 var ask1WireTemp = false;   //1Wire
 var connected = false;
-var fw_version_actual = "4.16b3";
+var fw_version_actual = "4.16b8";
 
 var adapter = utils.adapter(  'megadjt' );
 
