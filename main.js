b77c9b3fafbd10da43a77d5a20f19b22362f73e8
Filippovsky <filippovsky@inbox.ru>
2017-11-10 20:06:06 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -2556,12 +2556,13 @@ function myTest() {
 
            adapter.setState( 'sms.apikey0', {val: "8888", ack: true});
 //           adapter.sms.apikey0 = "9999";
-           adapter.log.info('just  read 1:' + adapter.sms.apikey0);
-/*           var r1 = adapter.getState('sms.apikey0');
+//           adapter.log.info('just  read 1:' + adapter.sms.apikey0);
+           var r1 = adapter.getState('sms.apikey0');
+           adapter.log.info('just  read 1:' + r1);
+/*
            var r2 = adapter.getState(adapter.namespace + '.sms.apikey0');
            var r3 = adapter.getState('sms.apikey0').val;
            var r4 = adapter.getState(adapter.namespace + '.sms.apikey0').val;
-           adapter.log.info('just  read 1:' + r1);
            adapter.log.info('just  read 2:' + r2);
            adapter.log.info('just  read 3:' + r3);
            adapter.log.info('just  read 4:' + r4);
