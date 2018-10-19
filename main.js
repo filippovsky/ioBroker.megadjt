162a038cfdcb566a723ce65d99614071999438a9
Filippovsky <filippovsky@inbox.ru>
2018-10-19 14:45:02 +0300

    v0.27.43 ALPHA
    Добавлена прошивка версии v4.30b2
    Добавлена прошивка версии v4.30b3
    Рефакторинг кода

--- main.js
+++ main.js
@@ -56,7 +56,7 @@ var ports  = {};
 ///var askInternalTemp = false;
 var ask1WireTemp = false;   //1Wire
 var connected = false;
-var fw_version_actual = "4.30b1";
+var fw_version_actual = "4.30b3";
 
 var adapter = utils.adapter(  'megadjt' );
 var sms_ru  = require('sms_ru');
