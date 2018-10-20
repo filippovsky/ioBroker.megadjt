f8c98738d418dc4c8e2818cb1c46de04524d00ba
Filippovsky <filippovsky@inbox.ru>
2018-10-20 11:29:45 +0300

    v0.28.4
    Добавлена прошивка версии v4.30b4

--- main.js
+++ main.js
@@ -55,7 +55,7 @@ var ports  = {};
 ///var askInternalTemp = false;
 var ask1WireTemp = false;   //1Wire
 var connected = false;
-var fw_version_actual = "4.30b3";
+var fw_version_actual = "4.30b4";
 
 var adapter = utils.adapter(  'megadjt' );
 var sms_ru  = require('sms_ru');
